9cec464d581df6d633c4d569ec69ce4cca89ab16
AliKarimi74 <AK-95@Alis-MacBook-Pro.local>
2015-05-07 18:37:43 +0430

    edit communication interfaces

--- Components/Communications/Interfaces/ClientCommunication.cs
+++ Components/Communications/Interfaces/ClientCommunication.cs
@@ -1,34 +1,52 @@
 using System;
+using System.Net.Sockets;
 
 namespace Communication
 {
 	enum Response {
-		SUCCESS,
-		FAIL,
-		WAITING,
-		NOT_RESPONSE
+		SUCCESS = 0,
+		FAIL = 1,
+		WAITING = 2,
+		NOT_RESPONSE = 3
 	}
 
 	class ClientCommunication
 	{
 		// necessary attributes
-		string DNS; //specify DNS(Server) ip
-		double TimeOut;
+		private Socket _clientSocket;
+		private string _ip;
+		private int _port; 
+		private double _timeOut;
+		private Response _response;
+		private Boolean _isAlive = true;
+		private RequestType lastRequest;
+
+		private DbCenter _dbClient;
+
+		/*
+		 * in constructor set DbCenter
+		 */
+		public ClientCommunication (DbCenter db);
 
 		/*
 		 * this function connect client to server.
 		 * return false if connected to server
 		 * 				else return true
-		 * sync function must start when we connect to datebase
+		 * new thread must create that in infinity loop check response events.
 		 */
 		public bool connect();
 
 		/*
 		 * this function disconnect from server and close current socket
-		 * alse destroy stuffs and free memory
 		 * return true if disconnecting is succesfully else return false
 		 */
 		public bool disconnect();
+
+		/* when user quit from program this function called
+		 * also destroy stuffs and free memory
+		 * isAlive parameter equals false;
+		 */
+		public void shutdown();
 
 		/*
 		 * this function is base of our comminications.
@@ -37,8 +55,10 @@ namespace Communication
 		 * for sending we must construct new thread and send message among it.
 		 * if we wait more than TimeOut and server dosen't response anything, FAIL response occured.
 		 * this funtion return response of our request
+		 * lastRequest should be update
+		 * this function communicate with client DbCenter
 		 */
-		public Response sendRequest(Request request);
+		public void sendRequest(Request request);
 
 		/*
 		 * this function upload file to server.
@@ -46,18 +66,32 @@ namespace Communication
 		 * if we wait more than TimeOut and server dosen't response anything, FAIL response occured.
 		 * during sending file to server, server response is WAITING. (in future we must grab send file process percent)
 		 */
-		public Response uploadFile(string fileLocation);
+		public void sendFile(string fileLocation);
 
 		/*
-		 * if client has a new happening this function called and receive this event.
-		 * when completely receive request from server, ask to server that disable new happening flag
+		 * if client has a new request or receive response this function called and receive this event.
+		 * when completely receive request from server, ask to server that disable new request flag
+		 * if response recieved response attribute changed.
+		 * if request received receivedRequest() function called.
 		 */
-		private void receiveRequest();
+		private void receiveCallBack(IAsyncResult AR);
 
 		/*
 		 * this function download a file from server and give it to DbCenter.
 		 * when client receive a new request which has files, this function called and receive files.
 		 */
-		private void downloadFile();
+		private void receiveFile();
+
+		/* 
+		 * when response state changed this function called.
+		 * this function do some appropriate action depend on received response.
+		 */
+		private void AnswerToResponse();
+
+		/*
+		 * this function do appropriate action depends on request type
+		 * also this function communicate with client DbCenter
+		 */
+		private void receivedRequest(Request newRequest);
 	}
 }
