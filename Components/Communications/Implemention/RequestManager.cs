47df099a6fe224095e5a698da0ababfee53f4c20
armin <karamzade.armin@gmail.com>
2015-05-15 18:02:38 +0430

    Request and ReqestManager incomplete.

---
+++ Components/Communications/Implemention/RequestManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_communication
+{
+
+    class RequestManager
+    {
+
+        //private DbCenter _dbcenter;
+        Response _response;
+        /*public RequestManager(DbCenter db)
+        {
+        }*/
+
+        public Response ExeRequest(Request req)
+        {
+            
+            return _response;
+        }
+
+        private Response NewReport()
+        {
+            
+            return _response;
+        }
+
+        private Response DeleteReport(int reportId)
+        {
+            return _response;
+        }
+
+        private Response MarkReport()
+        {
+            return _response;
+        }
+
+        private Response ReadRport()
+        {
+            return _response;
+        }
+
+    }
+}
