660deed5713b6b03e9d3c505ddb14119a8f1fcc6
armin <karamzade.armin@gmail.com>
2015-06-06 21:01:39 +0430

    Request Sub Sys views added but not complete.

---
+++ Components/Veiws Implementations/RequestSubSystem/RecievedRequest.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReqSubSys
+{
+    /// <summary>
+    /// Interaction logic for RecievedRequest.xaml
+    /// </summary>
+    public partial class RecievedRequest : Window
+    {
+        public RecievedRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void BackEvent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcceptEvent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RejectEvent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
