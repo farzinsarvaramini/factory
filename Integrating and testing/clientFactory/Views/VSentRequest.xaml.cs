31535402703f2870b620864d87ee875417bd312e
Ali Karimi <ali1374karimi@gmail.com>
2015-06-10 21:43:47 +0430

    add sent requet view + controller functions

--- Integrating and testing/clientFactory/Views/VSentRequest.xaml.cs
+++ Integrating and testing/clientFactory/Views/VSentRequest.xaml.cs
@@ -19,14 +19,64 @@ namespace clientFactory
     /// </summary>
     public partial class VSentRequest : Window
     {
+        public CRequestController controller;
+
+        public RequestModel current_request;
+
         public VSentRequest()
         {
             InitializeComponent();
         }
 
+        public void ShowForm()
+        {
+            Subject.Text = current_request.Title;
+            Send_Date.Text = current_request.SendDate.ToString();
+            Recipient.Text = current_request.Recipient;
+            Description.Text = current_request.Context;
+
+            float status = current_request.Status;
+            if (status == 0)
+                State.Content = "پذیرنده درخواست هنور پاسخی ارسال نکرده است";
+            else
+            {
+                if (status == 1)
+                    State.Content = "پذیرنده درخواست هنور پاسخی ارسال نکرده است";
+                else if ( status == -1 )
+                    State.Content = "پذیرنده درخواست هنور پاسخی ارسال نکرده است";
+                Answer.Text = current_request.Answer;
+            }
+
+            if (current_request.Follow)
+                Follow_Btn.IsEnabled = false;
+
+            Show();
+        }
+
         private void BackEvent_Click(object sender, RoutedEventArgs e)
         {
+            Close();
+        }
 
+        private void Follow_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            controller.FollowRequest();
+        }
+
+        public void ErrorMessage(string message)
+        {
+            MessageBox.Show(message,
+                "خطا",
+                MessageBoxButton.OK,
+                MessageBoxImage.Exclamation);
+        }
+
+        public void SuccessMessage(string message)
+        {
+            MessageBox.Show(message,
+                "نتیجه",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
         }
     }
 }
